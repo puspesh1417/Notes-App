dfa158b4e039be0122de8972cf04da5bed81d077
puspesh1417 <101013697+puspesh1417@users.noreply.github.com>
2022-08-08 17:38:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+console.log('welcome')
+let addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", function(e) {
+
+  let addTitle = document.getElementById("note-title");
+  let addTxt = document.getElementById("note-text");
+  
+    if (addTitle.value == "" || addTxt.value == "") {
+        return alert("Please add Note Title and Details")
+    }
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  addTitle.value = "";
+//   console.log(notesObj);
+  showNotes();
+});
+
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+        <div class="note" id="rfr">
+            <h5 class="note-counter">Note ${index + 1}</h5>
+            <h3 class="note-title"> ${element.title} </h3>
+            <p class="note-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Delete Note</button>
+            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit Note</button>
+           
+        </div>
+            `;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `No Notes Yet! Add a note using the form above.`;
+  }
+}
+
+// Function to delete a note
+function deleteNote(index) {
+
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
+  
+function MarkNote(index){
+  let notes = localStorage.getItem("rfr");
+  let b=document.getElementById("rfr")
+  document.body.style.backgroundColor = 'salmon';
+ 
+
+ localStorage.setItem("rfr");
+showNotes();
+ 
+}
+
+// Function to Edit the Note
+function editNote(index) {
+    let notes = localStorage.getItem("notes");
+    let addTitle = document.getElementById("note-title");
+    let addTxt = document.getElementById("note-text");
+
+    if (addTitle.value !== "" || addTxt.value !== "") {
+      return alert("Please clear the form before editing a note")
+    } 
+
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
+    console.log(notesObj);
+
+    notesObj.findIndex((element, index) => {
+      addTitle.value = element.title;
+      addTxt.value = element.text;
+    })
+    notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+}
+
+
+showNotes();
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function(){
+    let inputVal = search.value.toLowerCase();
+    let notesElm = document.getElementById("notes");
+
+     let noteCards = document.getElementsByClassName('note');
+     Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName("h3")[0].innerText;
+        let cardTxt1 = element.getElementsByTagName("p")[0].innerText;
+        // console.log(cardTxt)
+        if(cardTxt.includes(inputVal)||cardTxt1.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+          
+            element.style.display = "none";
+            
+            
+        }
+       
+     
+    //     // console.log(cardTxt);
+    })
+})
